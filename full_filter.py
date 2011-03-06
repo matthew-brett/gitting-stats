d8ec8b86df8706d9690d828be21cfe51455917fe
Matthew Brett <matthew.brett@gmail.com>
2011-03-06 13:27:50 -0800

    BF - fully functional conversion script

--- full_filter.py
+++ full_filter.py
@@ -9,7 +9,10 @@ import shutil
 from subprocess import call
 
 TO_RM = \
-"""doc
+"""nipy
+neuroimaging
+lib
+doc
 examples
 fff2.py
 libfffpy
@@ -17,15 +20,16 @@ setup_egg.py
 setup_fff2.py
 tools""".split('\n')
 
-TO_PRESERVE = \
-"""lib/statistics
-lib/neuroimaging/statistics
-lib/neuroimaging/algorithms/statistics
-neuroimaging/algorithms/statistics
-nipy/algorithms/statistics
-neuroimaging/fixes/scipy/stats_models
-neuroimaging/fixes/scipy/stats/models
-nipy/fixes/scipy/stats/models"""
+TO_PRESERVE = [
+    ('stats', ('lib/statistics',
+              'lib/neuroimaging/statistics',
+              'lib/neuroimaging/algorithms/statistics',
+              'neuroimaging/algorithms/statistics',
+              'nipy/algorithms/statistics')),
+    ('scipy_models', ('neuroimaging/fixes/scipy/stats_models',
+                     'neuroimaging/fixes/scipy/stats/models',
+                     'nipy/fixes/scipy/stats/models'))
+]
 
 
 def strip_trailing(pwd):
@@ -35,7 +39,7 @@ def strip_trailing(pwd):
             continue
         for fname in filenames:
             base, ext = splitext(fname)
-            if not ext in ('.py', '.pyx'):
+            if not ext in ('.py', '.pyx', '.txt', '.rst', '.c'):
                 continue
             newlines = []
             fullfile = pjoin(dirpath, fname)
@@ -56,12 +60,28 @@ def rm_list(to_rm):
 
 
 def preserve_list(to_pres):
-    pass
+    preserves = []
+    for outdir, sources in to_pres:
+        for source in sources:
+            if os.path.isdir(source):
+                preserves.append((outdir, source))
+                os.rename(source, outdir)
+                break
+    return preserves
+
+
+def restore_list(to_restore):
+    for outdir, source in to_restore:
+        assert os.path.isdir(outdir)
+        root, sdir = psplit(source)
+        os.makedirs(root)
+        os.rename(outdir, source)
 
 
 def main():
+    preserves = preserve_list(TO_PRESERVE)
     rm_list(TO_RM)
-    preserve_list(TO_PRESERVE)
+    restore_list(preserves)
     strip_trailing(os.getcwd())
 
 
